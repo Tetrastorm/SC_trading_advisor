729bb75150ba2640e3548ec7c310f90ea4a66c6f
Valentin Diard <valentin.diard@epitech.eu>
2018-12-24 18:59:54 +0100

    [ADD] - Display list - Add to list

--- source/main.cpp
+++ source/main.cpp
@@ -5,26 +5,37 @@
 
 using namespace std;
 
+string *add_to_list(string *list, string str, unsigned int size)
+{
+    string *tmp = list;
+    list = new string[size + 1];
+    for (unsigned int i = 0; i < size; i++)
+        list[i] = tmp[i];
+    list[size] = str;
+    return (list);
+}
+
 string *recover_content(string path)
 {
     string *content = nullptr;
     ifstream file(path);
     if (file.is_open()) {
         string line = "";
-        for (unsigned int i = 0; getline(file, line); i++) {
-            cout << line << '\n';
-            string *tmp = content;
-            content = new string[i+1];
-            for (unsigned int j = 0; j < i; j++)
-                content[j] = tmp[j];
-            content[i] = line;
-        }
+        for (unsigned int i = 0; getline(file, line); i++)
+            content = add_to_list(content, line, i);
         file.close();
         return (content);
     } else {
         perror("Error: File can't be open\n");
         exit(-1);
     }
+    return (content);
+}
+
+void display_list(string * const list)
+{
+    for (unsigned int i = 0; list[i] != "\0"; i++)
+        cout << list[i] << endl;
 }
 
 int main(int ac, char **av)
@@ -34,6 +45,5 @@ int main(int ac, char **av)
         exit(-1);
     }
     string *content = recover_content(av[1]);
-    for (unsigned int i = 0; i < 4; i++)
-        cout << content[i] << endl;
+    display_list(content);
 }
