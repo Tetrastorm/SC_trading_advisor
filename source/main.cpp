48e5f26f756150a13a6632b005b7fae6bdb04362
Valentin Diard <valentin.diard@epitech.eu>
2018-12-29 00:19:48 +0100

    [ADD] - Recover content from file

--- source/main.cpp
+++ source/main.cpp
@@ -14,31 +14,33 @@ typedef struct array_s {
 array_t *recover_content(char * const path)
 {
     string line;
-    array_t array = {0, 0, NULL};
+    array_t *array = new array_t;
     ifstream file;
 
     file.open(path);
-    array.array = new string*[1];
+    array->x = 0;
+    array->y = 0;
+    array->array = new string*;
     if (file.is_open()) {
         while (getline(file, line)) {
-            string *tmp = array.array[0];
-            array.x++;
-            array.array[0] = new string[array.x];
-            for (unsigned int i = 0; i < array.x - 1; i++)
-                array.array[0][i] = tmp[i];
-            array.array[0][array.x - 1] = line;
+            string *tmp = array->array[0];
+            array->x++;
+            array->array[0] = new string[array->x];
+            for (unsigned int i = 0; i < array->x - 1; i++)
+                array->array[0][i] = tmp[i];
+            array->array[0][array->x - 1] = line;
         }
         file.close();
     } else {
         perror("Error: File can't be open.\n");
         exit(-1);
     }
-    return (&array);
+    return (array);
 }
 
 void display(array_t *array)
 {
-    for (unsigned int i = 0; i < array->x; i++)
+    for (unsigned int i = 0; i < 5; i++)
         printf("%s\n", array->array[0][i].c_str());
 }
 
@@ -50,4 +52,6 @@ int main(int ac, char **av)
     }
     array_t *dataset = recover_content(av[1]);
     display(dataset);
+
+    delete[] dataset->array[0];
 }
